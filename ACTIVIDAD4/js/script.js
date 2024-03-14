d441366c89bd3ff0bebb79a63d387e2e2e33d53f
Rosalinh <151035942+Rosalinh@users.noreply.github.com>
2024-03-14 20:47:16 +0100

    Add files via upload

---
+++ ACTIVIDAD4/js/script.js
@@ -0,0 +1,10 @@
+function Calcular () {
+let a;
+let b;
+
+a = parseInt(prompt("Introduce el valor del primer  numero:"), 10);
+b = parseInt(prompt("Introduce el valor del segundo numero:"), 10);
+alert ("El primer número es:  " + a + "\nEl  segundo número es: " + b + "\nLa suma es: " + (a+b) + "\nLa resta es: " + (a-b) + "\nElproducto
+es: " + (a*b) + "\nLa división es: " + (a/b) +"\nEl modulo es: " + (a%b));
+
+}
