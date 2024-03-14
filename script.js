a5156ede4e158439718e80a85a6240f26ffdcc85
Rosalinh <151035942+Rosalinh@users.noreply.github.com>
2024-03-14 19:39:55 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function pedirDatos() {
-    let nombre;
-    nombre = prompt("¿ Como te llamas ?");
-    alert ("Hola " + nombre );
-    
-    let edad;
-    edad = prompt("¿Cuanto años tienes?")
-    alert ("Hola + nombre +", me han dicho que tienes " +  edad + "años,");")
-    }
