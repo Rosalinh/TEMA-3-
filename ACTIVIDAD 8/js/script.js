3075d982c79e66926ba1e895b6ce54e91f7b4008
Rosalinh <151035942+Rosalinh@users.noreply.github.com>
2024-03-14 22:12:05 +0100

    Add files via upload

---
+++ ACTIVIDAD 8/js/script.js
@@ -0,0 +1,39 @@
+function MensajeSwitch() {
+    let hora = prompt("¿Qué hora es?"),24);
+    switch (respuesta) {
+     case "6":
+     case "7":
+     case "8":
+     case "9":
+     case "10":
+     case "11":
+     case "12":
+       alert("Buenos días");
+     break;
+     case "13":
+     case "14":
+     case "15":
+     case "16":
+     case "17":
+     case "18":
+     case "19":
+     case "20":
+         alert("Buenas tardes");
+     break;
+     case "21":
+     case "22":
+     case "23":
+     case "00":
+     case "1":
+     case "2":
+     case "3":
+     case "4":
+     case "5":
+
+       alert ("Buenas noches");
+      break;
+      default:
+        alert("El número introducido no es válido");
+        break;
+    }
+  }
